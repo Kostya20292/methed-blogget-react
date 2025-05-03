6cec1ae277e59b65c05f902758ddf647fe39a0fb
Konstantin Makarevich <mak.kostya2029@gmail.com>
2025-05-03 15:13:40 +0300

    feat: Add component header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import { Layout } from '../Layout/Layout';
+import { Logo } from './Logo/Logo';
+import { Heading } from './Heading/Heading';
+import { Search } from './Search/Search';
+import { Auth } from './Auth/Auth';
+
+import style from './Header.module.css';
+
+export const Header = () => {
+  return (
+    <header className={style.header}>
+      <Layout>
+        <div className={style.gridContainer}>
+          <Logo />
+          <Heading text="Главная" />
+          <Search />
+          <Auth auth="" />
+        </div>
+      </Layout>
+    </header>
+  );
+};
