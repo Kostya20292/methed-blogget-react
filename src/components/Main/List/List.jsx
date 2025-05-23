5801aae7fed9c03e6374e159d18d3b9a7986f0d4
Konstantin Makarevich <mak.kostya2029@gmail.com>
2025-05-23 17:47:06 +0300

    feat: Add tabs and new posts

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -3,17 +3,38 @@ import { Post } from './Post/Post';
 import style from './List.module.css';
 
 export const List = () => {
-  const postData = {
-    thumbnail: '',
-    title: 'Title',
-    author: 'Author',
-    ups: 24,
-    date: '2022-02-23T09:45:00.000Z',
-  };
+  const postsData = [
+    {
+      id: 1,
+      thumbnail: '',
+      title: 'Title1',
+      author: 'Author1',
+      ups: 24,
+      date: '2022-02-23T09:45:00.000Z',
+    },
+    {
+      id: 2,
+      thumbnail: '',
+      title: 'Title2',
+      author: 'Author2',
+      ups: 24,
+      date: '2022-02-23T09:45:00.000Z',
+    },
+    {
+      id: 3,
+      thumbnail: '',
+      title: 'Title3',
+      author: 'Author3',
+      ups: 24,
+      date: '2022-02-23T09:45:00.000Z',
+    },
+  ];
 
   return (
     <ul className={style.list}>
-      <Post postData={postData} />
+      {postsData.map((data) => (
+        <Post key={data.id} postData={data} />
+      ))}
     </ul>
   );
 };
--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,18 +1,82 @@
+import { useEffect, useState } from 'react';
+import { PropTypes } from 'prop-types';
+
+import { generateRandomId } from '../../../utils/generateRandomId.js';
+import { debounceRaf } from '../../../utils/debounce';
+
+import { ReactComponent as HomeIcon } from './img/home.svg';
+import { ReactComponent as TopIcon } from './img/top.svg';
+import { ReactComponent as BestIcon } from './img/best.svg';
+import { ReactComponent as HotIcon } from './img/hot.svg';
+import { ReactComponent as ArrowIcon } from './img/arrow.svg';
+
 import style from './Tabs.module.css';
 
-export const Tabs = () => (
-  <ul className={style.list}>
-    <li>
-      <a href="/">Главная</a>
-    </li>
-    <li>
-      <a href="/">Просмотренные</a>
-    </li>
-    <li>
-      <a href="/">Сохраненные</a>
-    </li>
-    <li>
-      <a href="/">Мои посты</a>
-    </li>
-  </ul>
-);
+const LIST = [
+  { value: 'Главная', Icon: HomeIcon },
+  { value: 'Топ', Icon: TopIcon },
+  { value: 'Лучшие', Icon: BestIcon },
+  { value: 'Горячие', Icon: HotIcon },
+].map((item) => ({ ...item, id: generateRandomId() }));
+
+export const Tabs = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isDropDown, setIsDropDown] = useState(true);
+  const [dropDownTitle, setDropDownTitle] = useState('Главная');
+
+  const handleResize = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setIsDropDown(true);
+    } else {
+      setIsDropDown(false);
+    }
+  };
+
+  useEffect(() => {
+    const debounceResize = debounceRaf(handleResize);
+
+    debounceResize();
+
+    window.addEventListener('resize', debounceResize);
+
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
+
+  const handleClick = (e) => {
+    setDropDownTitle(e.target.textContent);
+    setIsDropDownOpen(false);
+  };
+
+  return (
+    <div className={style.container}>
+      {isDropDown && (
+        <div className={style.wrapperBtn}>
+          <button className={style.btn} onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
+            {dropDownTitle}
+            <ArrowIcon width={15} height={15} />
+          </button>
+        </div>
+      )}
+
+      {(isDropDownOpen || !isDropDown) && (
+        <ul className={style.list} onClick={handleClick}>
+          {LIST.map(({ value, id, Icon }) => (
+            <li className={style.item} key={id}>
+              <button className={style.btn}>
+                {value}
+                {Icon && <Icon width={30} height={30} />}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+Tabs.propTypes = {
+  list: PropTypes.array,
+  setList: PropTypes.func,
+};
